815efc86f1a36cfd4cd509cc57f8a46893e3bc0a
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2026-04-16 13:00:17 -0500

    Fix indentation and improve board dimension display

--- Tablero.py
+++ Tablero.py
@@ -10,9 +10,10 @@ with st.sidebar:
     st.subheader("Dimensiones del Tablero")
     st.write("Ancho del tablero:")
     canvas_width =  600 #st.slider("Ancho del tablero", 300, 700, 500, 50)
-    st.write("Alto del tablero:")
+    
     canvas_height = 400 #st.slider("Alto del tablero", 200, 600, 300, 50)
     st.write(canvas_width)
+    st.write("Alto del tablero:")
     st.write(canvas_height)
     
     # Drawing mode selector
